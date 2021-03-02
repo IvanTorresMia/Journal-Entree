252bf20c9e4c3b5e15d1fa10aea2c6a911a3ce92
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-02 15:28:07 -0800

    created login button

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './Components/LoginButton'
 
 function App() {
   return (
     <div>
-     
+     <LoginButton />
     </div>
-  );
+  ); 
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 
-
 ReactDOM.render(
   <Auth0Provider
   domain={domain}
