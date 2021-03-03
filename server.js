f758b1ccf934304bc2483be188e962abc7ff44f5
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-02 19:25:57 -0800

    connected to database

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-// const routes = require("./routes/index.js")
+const routes = require("./routes")
 
 const app = express();
  
 // listening on Port 3001 on server side.
 const PORT = process.env.PORT || 3001;
+
+const db = require("./models")
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +16,10 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 }
 
-app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-})
+db.sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+
 
