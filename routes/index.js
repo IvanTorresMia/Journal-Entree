fca7df17b16e2bee28688c3dfc22a483f09ed7eb
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-02 19:22:52 -0800

    created route for user on back end

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+const router = require("express").Router();
+
+router.get("/api/User", (req, res) => {
+    db.User.create({ 
+        email: req.body.email
+    }).then((dbUser) => {
+        res.json(dbUser);
+    }).catch(err => res.status(422).json(err))
+})
+
+module.exports = router;
