5b13ac7a581fb13407a09248bc6da68a1539cce5
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-02 18:06:06 -0800

    setting up User authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import LoginButton from './Components/LoginButton'
+import "./App.css";
+import LoginButton from "./Components/LoginButton";
+import LogOutButton from "./Components/LogOutButton";
+import Profile from "./Components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <div>
-     <LoginButton />
+      <LoginButton />
+      <LogOutButton />
+      <Profile />
     </div>
-  ); 
+  );
 }
 
 export default App;
