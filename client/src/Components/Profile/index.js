67f270c431a45a756af03cd7b4c3b929919e7da6
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-02 18:12:04 -0800

    setting up back end express and package.json

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -4,7 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
 
+
+console.log(user.email)
+
   return (
+
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
