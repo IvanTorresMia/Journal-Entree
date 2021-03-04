cf6d0df5c362d0ab255dab33ed1e1938e0282034
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-03 19:51:58 -0800

    created header component

---
+++ client/src/Components/LoginComp/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="jumbotron header-wraper">
+      <div className="container">
+       
+      </div>
+    </div>
+  );
+};
+
+export default Header;
