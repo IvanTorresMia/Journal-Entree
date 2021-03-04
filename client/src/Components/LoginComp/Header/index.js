8982ab2b565fc5133dd99202b067497c579f6bb2
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-03 20:12:37 -0800

    building header with img and col

--- client/src/Components/LoginComp/Header/index.js
+++ client/src/Components/LoginComp/Header/index.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Images from '../../../Utils/loginImages'
 
 const Header = () => {
   return (
     <div className="jumbotron header-wraper">
       <div className="container">
-       
+        <div className="row">
+          <div className="col">
+              <img className="header-img rounded" src={Images.img} alt="login journal"/>
+          </div>
+          <div className="col">
+              <h1>Welcome</h1>
+              <p>Journaling can literally change your life</p>
+          </div>
+        </div>
       </div>
     </div>
   );
