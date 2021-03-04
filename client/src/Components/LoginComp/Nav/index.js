ee6c718563affe9caa099d5e1df85865eb1d1c08
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-03 19:52:14 -0800

    created nav component

---
+++ client/src/Components/LoginComp/Nav/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const Nav = ({ handleLogin }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+         JournalEntree
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+               About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a onClick={handleLogin} className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
