143cfd46406e44ec287732f5a1973063be06968d
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 15:00:14 -0800

    adding auth0 to about us page

--- client/src/Pages/AboutUs/index.js
+++ client/src/Pages/AboutUs/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Nav from '../../Components/LoginComp/Nav'
 import AboutMe from '../../Components/LoginComp/AboutMe'
+import { useAuth0 } from "@auth0/auth0-react";
 
 const AboutUs = () => {
-
+    const { loginWithRedirect } = useAuth0();
     return (
         <div>
             <Nav handleLogin={() => loginWithRedirect()} />
