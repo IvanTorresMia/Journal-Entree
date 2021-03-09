4e24c4d28fab2d8184f57b474ca0399484603cf7
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 14:59:05 -0800

    added about us and made it functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import {
+  BrowserRouter as Router,
+  // Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Login from "./Pages/Login";
 import Profile from "./Pages/Profile";
+import AboutUs from "./Pages/AboutUs";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const App = () => {
-  const { isLoading, isAuthenticated, loginWithRedirect, logout } = useAuth0();
+  const { isLoading, isAuthenticated, logout } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
   }
 
   return (
-    <div>
-      {isAuthenticated ? (
-        <Profile handleLogout={() => logout()} />
-      ) : (
-        <Login />
-      )}
-    </div>
+    <Router>
+      <div>
+        {isAuthenticated ? (
+          <Profile handleLogout={() => logout()} />
+        ) : (
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Login} />
+            <Route exact path="/aboutUs" component={AboutUs} />
+          </Switch>
+        )}
+      </div>
+    </Router>
   );
 };
 
