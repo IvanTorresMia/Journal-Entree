ffb0356503ebeac5c81327813e601faac6ec4911
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 00:18:51 -0800

    created card for reasons comp

--- client/src/Components/LoginComp/Reasons/Card/index.js
+++ client/src/Components/LoginComp/Reasons/Card/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ data }) => {
     return (
-        
-        <div>
 
+        <div> 
+            <img src={data.img} alt="reason" className="reason-img" />
+            <p>{data.reason}</p>
         </div>
     )
 }
