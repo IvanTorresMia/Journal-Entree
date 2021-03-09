3a164e3daf5743cb0d0ac16187e542724819a8ac
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 15:01:39 -0800

    reformation api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,6 +5,9 @@ const API = {
   createUser: function (email) {
     return axios.post("/api/User", email);
   },
+  getUser: function() {
+    return axios.get("/api/User")
+},
 };
 
 export default API;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const db = require("../models");
+const path = require("path");
 const router = require("express").Router();
+
 
 router.post("/api/User", (req, res) => {
     db.User.create({ 
@@ -8,5 +10,12 @@ router.post("/api/User", (req, res) => {
         res.json(dbUser);
     }).catch(err => res.status(422).json(err))
 })
+
+
+// if no api routes are hit then we send the react app.
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
+
 
 module.exports = router;
