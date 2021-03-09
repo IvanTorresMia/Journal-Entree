647b52eeba01eeff9a6ba071050aa1a9d727cfd3
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-08 22:56:43 -0800

    working on header component

--- client/src/Components/LoginComp/Header/index.js
+++ client/src/Components/LoginComp/Header/index.js
@@ -1,17 +1,27 @@
 import React from "react";
-import Images from '../../../Utils/loginImages'
+import Images from "../../../Utils/loginImages";
 
-const Header = () => {
+const Header = ({ handleLogin }) => {
   return (
     <div className="jumbotron header-wraper">
       <div className="container">
         <div className="row">
-          <div className="col">
-              <img className="header-img rounded" src={Images.img} alt="login journal"/>
+          <div className="col img-wraper">
+            <img
+              className="header-img rounded"
+              src={Images.img}
+              alt="login journal"
+            />
           </div>
           <div className="col">
-              <h1>Welcome</h1>
-              <p>Journaling can literally change your life</p>
+            <h1>Welcome</h1>
+            <p>
+              Journaling has personaly impacted my life. So I decided to create
+              a way for you to Journal your life.
+            </p>
+
+            <button className="btn btn1">Join Now</button>
+            <button className="btn btn2">Who Built It?</button>
           </div>
         </div>
       </div>
