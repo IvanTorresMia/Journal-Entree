1f8b9691b63d5e071fdcfba6f5f063b1c1391420
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 00:18:39 -0800

    mapping through thr reason cards

--- client/src/Components/LoginComp/Reasons/index.js
+++ client/src/Components/LoginComp/Reasons/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Card from './Card'
+import data from '../../../Utils/Reasons'
 
 
 const Reasons = () => {
 
     return (
         <div>
-
+            {data.map((reason, i) => (
+                <Card data={reason} key={i}/>
+            ))}
         </div>
     )
 }
