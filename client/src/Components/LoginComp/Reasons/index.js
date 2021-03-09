90b1a0a75d0a5de96d3e2949c223a20c95f1b2e7
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 00:39:06 -0800

    imported card and edited jsx

--- client/src/Components/LoginComp/Reasons/index.js
+++ client/src/Components/LoginComp/Reasons/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Card from './Card'
-import data from '../../../Utils/Reasons'
-
+import React from "react";
+import Card from "./Card";
+import data from "../../../Utils/Reasons";
 
 const Reasons = () => {
-
-    return (
-        <div>
-            {data.map((reason, i) => (
-                <Card data={reason} key={i}/>
-            ))}
+  return (
+    <div className="jumbotron reasons-wraper">
+      <div className="container">
+          <h1>So Why Journal?</h1>
+        <div className="row reasons-container">
+          {data.map((reason, i) => (
+            <Card data={reason} key={i} />
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Reasons;
