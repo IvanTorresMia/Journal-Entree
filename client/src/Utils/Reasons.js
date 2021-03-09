fce8920fb1a1444317bbced1a98b13828514fcdf
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 00:19:33 -0800

    reasons data file

--- client/src/Utils/Reasons.js
+++ client/src/Utils/Reasons.js
@@ -1,14 +1,17 @@
-
+import logo from '../loginImages/nature.jpg';
 
 const Reasons = [
 {
-    reason: "Helps your process"
+    reason: "Helps your process",
+    img: logo
 },
 {
-    reason: "Privacy is key"
+    reason: "Privacy is key",
+    img: logo
 },
 {
-    reason: "Proven to act as a form of therapy"
+    reason: "Proven to act as a form of therapy",
+    img: logo
 }
 ]
 
