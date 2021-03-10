d28722326bddbec33a204dd80b2fc0f580776b7b
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 20:33:44 -0800

    building working on routes for client and backend

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-
-
-
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import API from "../../Utils/API";
 
 const Profile = ({ handleLogout }) => {
+  const { user, isAuthenticated } = useAuth0();
 
-    return (
-<div>
-<h1>Logout</h1>
+  if (isAuthenticated) {
+    const email = JSON.stringify(user.email);
+    console.log(email);
+    API.createUser({ email: email }).then((res) => {
+      console.log(res);
+    });
+  }
 
+  return (
+    isAuthenticated && (
+      <div>
+        <h1>Logout</h1>
 
-<button onClick={handleLogout}>LOGOUT</button>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
 
-</div>
+        {JSON.stringify(user, null, 2)}
+        <button onClick={handleLogout}>LOGOUT</button>
+      </div>
     )
-}
+  );
+};
 
 export default Profile;
--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,25 @@ const db = require("../models");
 const path = require("path");
 const router = require("express").Router();
 
-
+// Route to Create a user
 router.post("/api/User", (req, res) => {
-    db.User.create({ 
-        email: req.body.email
-    }).then((dbUser) => {
-        res.json(dbUser);
-    }).catch(err => res.status(422).json(err))
-})
+  db.User.create({
+    email: req.body.email,
+  })
+    .then((dbUser) => {
+      res.json(dbUser);
+    })
+    .catch((err) => res.status(422).json(err));
+});
 
+// Route to get user
+router.get("/api/User", (req, res) => {
+  res.json({ email: req.user.email, id: req.user.id });
+});
 
 // if no api routes are hit then we send the react app.
-router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-  });
-
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = router;
