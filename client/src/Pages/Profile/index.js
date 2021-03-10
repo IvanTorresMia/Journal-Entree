797a8a2e4a719221ca59369a590b067003c03ac6
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-10 13:15:02 -0800

    working on login function post auth

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import API from "../../Utils/API";
 
 const Profile = ({ handleLogout }) => {
   const { user, isAuthenticated } = useAuth0();
-  const [userData, setUserData] = useState({});
+  const [userEmail, setUserEmail] = useState();
 
-  if (isAuthenticated) {
-    const email = JSON.stringify(user.email);
-    console.log(email);
-    API.createUser({ email: email }).then((res) => {
-      console.log(res);
+  useEffect(() => {
+    getCreateOrGet();
+  }, []);
+
+  // function checks if user exist in data base, if not then it creates one
+  const getCreateOrGet = () => {
+    API.getUser().then((res) => {
+      const users = [];
+      for (let i = 0; i < res.data.length; i++) {
+        users.push(res.data[i].email);
+      }
+
+      if (users.includes(JSON.stringify(user.email))) {
+        setUserEmail(user.email);
+        console.log(user.email + " is already created");
+      } else {
+        if (isAuthenticated) {
+          const email = JSON.stringify(user.email);
+          API.createUser({ email: email }).then((res) => {
+            console.log(res);
+          });
+        }
+      }
     });
-  }
-
-  const handleUserClick = () => {
-    //   const email = user.email;
-      API.getUser().then(res => {
-          console.log(res)
-      })
   };
 
   return (
@@ -29,8 +40,6 @@ const Profile = ({ handleLogout }) => {
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-
-        <button onClick={handleUserClick}>Get Email</button>
         <br />
         {JSON.stringify(user, null, 6)}
         <br />
