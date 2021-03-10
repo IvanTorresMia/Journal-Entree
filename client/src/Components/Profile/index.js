ff934c6d4036be7fd3227565f6f72e30d7196993
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-09 23:10:46 -0800

    testing data response on client side

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import API from "../../Utils/API.js";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
+  const [ userData, setUserData ] = useState({})
 
   if (isAuthenticated) {
     const email = JSON.stringify(user.email);
     console.log(email);
     API.createUser({ email: email }).then((res) => {
       console.log(res);
+      setUserData(res)
     });
   }
 
@@ -20,7 +22,7 @@ const Profile = () => {
         <h2>{user.name}</h2>
         <p>{user.email}</p>
       
-
+        <p>{userData}</p>
         {JSON.stringify(user, null, 2)}
       </div>
     )
