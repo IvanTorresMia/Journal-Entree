c65d3408f77689520719cbed3a3e0ac2092130e6
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-22 19:56:48 -0700

    new client side api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,26 +1,31 @@
 import axios from "axios";
 
-// eslint-disable-next-line 
+// eslint-disable-next-line
 const API = {
   createUser: function (email) {
     return axios.post("/api/User", email);
   },
-  getUsers: function() {
-    return axios.get("/api/User/all")
-},
-getUser: function(id) {
-  return axios.get("/api/User/One", id)
-},
-  createEntry: function(title, text, id) {
+  createUserName: (userName) => {
+    return axios.post("/api/User", userName)
+  },
+  getUsers: function () {
+    return axios.get("/api/User/all");
+  },
+  getUser: function (id) {
+    return axios.get("/api/User/One", id);
+  },
+  createEntry: function (title, text, id) {
     return axios.post("/api/Entry", title, text, id);
   },
-  getEntries: function() {
+  getEntries: function () {
     return axios.get("/api/Entry");
+  },
+  createCatagory: (name, description, id) => {
+    return axios.post("/api/Catagory", name, description, id)
   }
 };
 
 export default API;
 
-
 // You actually don't fully know how to make this work hahaha
-// but you will bb boy. 
+// but you will bb boy.
