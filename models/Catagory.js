1fc5f432e9f50c35921c3ded7da14c829e054b8d
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-22 19:57:02 -0700

    created catagory model

---
+++ models/Catagory.js
@@ -0,0 +1,21 @@
+const { STRING } = require("sequelize");
+const sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Catagory = sequelize.define("Catagory", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 20]
+            }
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    })
+
+    return Catagory;
+
+}
