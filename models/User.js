a15c948e62d987631fdc5785d6ab2a3f51165cf7
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-22 20:21:31 -0700

    fixing relations for user model

--- models/User.js
+++ models/User.js
@@ -1,26 +1,21 @@
 module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 
-    const User = sequelize.define("User", {
-
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
+  User.associate = function (models) {
+    // Using relational data. User will have many entries, if user is deleted then they all delete.
+    User.hasMany(models.Catagory, {
+      onDelete: "cascade",
     });
 
-    User.associate = function(models) {
-   
-        // Using relational data. User will have many entries, if user is deleted then they all delete.
-        User.hasMany(models.JournalEntry, {
-            onDelete: "cascade"
-        });
-
-        User.hasOne(models.Profile), {
-            onDelete: "cascade"
-        }
-
-
+    User.hasOne(models.Profile),
+      {
+        onDelete: "cascade",
       };
-    return User;
-}
+  };
+  return User;
+};
