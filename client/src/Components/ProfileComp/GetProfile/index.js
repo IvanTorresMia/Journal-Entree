c29df8aec0da3da6304effdb2a5aba3431f7177e
Ivan Torres <ivantorresmia27@gmail.com>
2021-03-30 21:55:59 -0700

    cleaning up code after working

--- client/src/Components/ProfileComp/GetProfile/index.js
+++ client/src/Components/ProfileComp/GetProfile/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-const GetProfile = ({ email, handlePrfileInput, handleProfileClick}) => {
+const GetProfile = ({ email, handlePrfileInput, handleProfileClick }) => {
   return (
     <div className="jumbotron getProfile-wraper">
       <div className="container GetProfile-container">
-          <h1>Welcome <span className="email">{email}</span> !</h1>
-          <p>Before we start, What's your Name?</p>
-          <input onChange={handlePrfileInput} placeholder="name" />
-          <button className="btn" onClick={handleProfileClick}>Begin Writing</button>
+        <h1>
+          Welcome <span className="email">{email}</span> !
+        </h1>
+        <p>Before we start, What's your Name?</p>
+        <input onChange={handlePrfileInput} placeholder="name" />
+        <button className="btn" onClick={handleProfileClick}>
+          Begin Writing
+        </button>
       </div>
     </div>
   );
