c37f73c3aea623f603922c649eb9e724f8403019
Ivan Torres <ivantorresmia27@gmail.com>
2021-04-13 12:07:30 -0700

    created journal form

--- client/src/Components/ProfileComp/EntryComponents/Wrapper/index.js
+++ client/src/Components/ProfileComp/EntryComponents/Wrapper/index.js
@@ -1,15 +1,33 @@
-import { DialogTitle } from '@material-ui/core';
-import React from 'react';
-
+import { DialogTitle } from "@material-ui/core";
+import React from "react";
 
 const JournalWrapper = ({ title }) => {
-
-    console.log(title)
-    return (
-        <div>
-            {title}
+  console.log(title);
+  return (
+    <div className="container entry-form">
+      <div className="row">
+        <div className="col">{title}</div>
+        <div className="col">
+          <div className="container">
+            <div className="form">
+              {" "}
+              <input type="text" name="title" autoComplete="off" required />
+              <label htmlFor="title" className="label-title">
+                <span className="content-title"> title </span>
+              </label>
+            </div>
+            <div className="form-entry">
+              {" "}
+              <input type="text" name="title" autoComplete="off" required />
+              <label htmlFor="entry" className="label-entry">
+                <span className="content-entry">Entry</span>
+              </label>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default JournalWrapper;
