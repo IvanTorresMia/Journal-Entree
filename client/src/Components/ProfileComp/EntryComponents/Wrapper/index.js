d1f58a550ca2d17b408b4bbcd2ed7410b38c83b4
Ivan Torres <ivantorresmia27@gmail.com>
2021-04-14 14:25:01 -0700

    working on journal wrapper and editing for styling

--- client/src/Components/ProfileComp/EntryComponents/Wrapper/index.js
+++ client/src/Components/ProfileComp/EntryComponents/Wrapper/index.js
@@ -1,28 +1,55 @@
-import { DialogTitle } from "@material-ui/core";
 import React from "react";
+import Entries from "../Entries";
 
-const JournalWrapper = ({ title }) => {
+const JournalWrapper = ({
+  title,
+  JournalEntries,
+  handleSaveClick,
+  handleTileChange,
+  handleBodyChange,
+}) => {
   console.log(title);
+console.log(JournalEntries)
   return (
-    <div className="container entry-form">
+    <div className="container journal-con">
       <div className="row">
-        <div className="col">{title}</div>
         <div className="col">
-          <div className="container">
+          <h2>{title}</h2>
+          {JournalEntries.map((entry, i) => (
+            <Entries JournalEntries={entry.title} key={i}/>
+          ))}
+        </div>
+        <div className="col">
+          <div className="container entry-form">
             <div className="form">
               {" "}
-              <input type="text" name="title" autoComplete="off" required />
+              <input
+                onChange={handleTileChange}
+                type="text"
+                name="title"
+                autoComplete="off"
+                required
+              />
               <label htmlFor="title" className="label-title">
-                <span className="content-title"> title </span>
+                <span className="content-title"> Title </span>
               </label>
             </div>
             <div className="form-entry">
               {" "}
-              <input type="text" name="title" autoComplete="off" required />
+              <textarea
+                onChange={handleBodyChange}
+                type="text"
+                name="title"
+                autoComplete="off"
+                required
+              ></textarea>
               <label htmlFor="entry" className="label-entry">
                 <span className="content-entry">Entry</span>
               </label>
             </div>
+            <a className="save-entry" onClick={handleSaveClick}>
+              SAVE
+            </a>
           </div>
         </div>
       </div>
