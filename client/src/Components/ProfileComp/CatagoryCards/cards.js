be1ef808950ca4e45e307e7ec5401394bac65e5a
Ivan Torres <ivantorresmia27@gmail.com>
2021-04-14 14:24:11 -0700

    editing jsx for styling

--- client/src/Components/ProfileComp/CatagoryCards/cards.js
+++ client/src/Components/ProfileComp/CatagoryCards/cards.js
@@ -11,9 +11,9 @@ const Cards = ({ title, description, tempImage, handleJournalClick }) => {
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href="#" className="btn btn-primary" >Delete</a>
+    <a href="#" className="rounded" >Delete</a>
     <p className="display-con">{title}</p>
-    <a href="#" className="btn btn-primary" onClick={handleJournalClick}>Journal</a>
+    <a href="#" className="rounded" onClick={handleJournalClick}>Journal</a>
   </div>
 </div>
   );
