f94237280bfa7af7e71014364bd592449bea7dd4
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-16 09:31:20 -0700

    editing models for tagline

--- models/Profile.js
+++ models/Profile.js
@@ -4,6 +4,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    tagline: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   });
 
   Profile.associate = (models) => {
