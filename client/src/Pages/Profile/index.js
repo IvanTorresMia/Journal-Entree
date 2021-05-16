f8f92308d940b13bc1df2d01b05d99a4cd1030ef
Ivan Torres <ivantorresmia27@gmail.com>
2021-05-16 09:33:07 -0700

    refactoring profile and fixed Journal function

--- client/src/Pages/Profile/index.js
+++ client/src/Pages/Profile/index.js
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./profile.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import Wrapper from "../../Components/ProfileComp/Wrapper";
 import API from "../../Utils/API";
-import { BrowserRouter as Route } from "react-router-dom";
-import JournalWrapper from "../../Components/ProfileComp/EntryComponents/Wrapper";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 /* --------------------- Components -------------------*/
 import Nav from "../../Components/ProfileComp/NavBar";
 import GetProfile from "../../Components/ProfileComp/GetProfile";
+import JournalWrapper from "../../Components/ProfileComp/EntryComponents/Wrapper";
+import Wrapper from "../../Components/ProfileComp/Wrapper";
 
 // function to handle log out.
 const Profile = ({ handleLogout }) => {
   const { user } = useAuth0();
   const [profile, setProfile] = useState({
     userName: "",
+    tagLine: "",
   });
   const [gotUserName, setGotUserName] = useState(false);
   const [journaling, setJournaling] = useState(false);
@@ -35,8 +41,8 @@ const Profile = ({ handleLogout }) => {
   }, []);
 
   useEffect(() => {
-    console.log(entryList);
-  }, [journaling]);
+    console.log(gotUserName);
+  }, []);
 
   /* --------------------- Functions for User -------------------*/
 
@@ -44,14 +50,13 @@ const Profile = ({ handleLogout }) => {
   const CreateOrGet = () => {
     API.getUsers().then((res) => {
       const users = [];
-      console.log(res);
+      // console.log(res);
       for (let i = 0; i < res.data.length; i++) {
         users.push(res.data[i].email);
       }
       if (!users.includes(user.email)) {
         const email = user.email;
         API.createUser({ email: email }).then((response) => {
-
           API.getUsers().then((secondRes) => {
             for (let j = 0; j < secondRes.data.length; j++) {
               if (secondRes.data[j].email == user.email) {
@@ -79,11 +84,17 @@ const Profile = ({ handleLogout }) => {
   const handleJournalClick = (event) => {
     const currentValue = event.target.previousElementSibling.innerHTML;
     const email = user.email;
-    API.getUser(email).then((res) => {
+    getCatagoryInfo(email, currentValue);
+    setJournaling(true);
+    setGotUserName(false);
+    console.log(journaling);
+    console.log(gotUserName);
+  };
 
+  const getCatagoryInfo = (email, currentValue) => {
+    API.getUser(email).then((res) => {
       API.getCatagory(currentValue, res.data.id).then((res) => {
         setCurrentJournal({ ...currentJournal, title: res.data.name });
-        setJournaling(true);
         setEntry({ ...entry, id: res.data.id });
         getAllCatagories(res.data.id);
       });
@@ -102,35 +113,81 @@ const Profile = ({ handleLogout }) => {
   };
 
   const handleSaveClick = () => {
-    API.createEntry({
-      title: entry.title,
-      text: entry.body,
-      id: entry.id,
-    }).then((res) => {
-      console.log(res);
-      console.log(entryList);
+    const email = user.email;
+    const currentValue = currentJournal.title;
+    API.getUser(email).then((res) => {
+      API.getCatagory(currentValue, res.data.id).then((res) => {
+        API.getAllEntries(res.data.id).then((res) => {
+          console.log(res);
+          console.log(res.data);
+          console.log(entry.title);
+
+          console.log(res.data.length);
+
+          if (res.data.length === 0) {
+            console.log("this works");
+            API.createEntry({
+              title: entry.title,
+              text: entry.body,
+              id: entry.id,
+            }).then((res) => {
+              console.log(res);
+              console.log(entryList);
+              // setRefresh(true);
+            });
+            getCatagoryInfo(user.email, currentJournal.title);
+          } else {
+            let journals = [];
+
+            for (let i = 0; i < res.data.length; i++) {
+              journals.push(res.data[i].title);
+              if (res.data[i].title == entry.title) {
+                //we update
+                console.log("make an update call");
+
+                return;
+              } else {
+                console.log("this works");
+                API.createEntry({
+                  title: entry.title,
+                  text: entry.body,
+                  id: entry.id,
+                }).then((res) => {
+                  console.log(res);
+                  console.log(entryList);
+                });
+
+                getCatagoryInfo(user.email, currentJournal.title);
+                return;
+              }
+            }
+          }
+        });
+      });
     });
   };
 
   const getAllCatagories = (id) => {
     API.getAllEntries(id).then((res) => {
-      console.log(res.data);
+      // console.log(res.data);
       setEntryList(res.data);
     });
   };
-
-
 
   /* --------------------- functions for Prifile -------------------*/
 
   const submitProfile = (event) => {
     event.preventDefault();
     const userName = profile.userName;
+    const tagLine = profile.tagLine;
     const id = ids;
-    API.createProfile({ userName: userName, id: id }).then((res) => {
-      setGotUserName(true);
-      createCatagory("Notes", "Main Notes here", id);
-    });
+    API.createProfile({ userName: userName, tagLine: tagLine, id: id }).then(
+      (res) => {
+        setGotUserName(true);
+        console.log(res);
+        createCatagory("Notes", "Main Notes here", id);
+      }
+    );
   };
 
   const handleUserName = (event) => {
@@ -138,80 +195,52 @@ const Profile = ({ handleLogout }) => {
     setProfile({ ...profile, userName: value });
   };
 
+  const handleTagLineChange = (event) => {
+    const { value } = event.target;
+    setProfile({ ...profile, tagLine: value });
+  };
+
   return (
-    <>
+    <Router>
       <Nav handleLogout={handleLogout} />
       <div className="container profile-body">
-        <Route exact path="/">
-          {gotUserName ? (
-             <>
-              <Wrapper
-                handleJournalClick={handleJournalClick}
-              />
-              <JournalWrapper
-              title={currentJournal.title}
-              handleTileChange={handleTileChange}
-              handleBodyChange={handleBodyChange}
-              handleSaveClick={handleSaveClick}
-              JournalEntries={entryList}
-            />
-            </>
+        <Switch>
+          <Route exact path="/">
+            {!gotUserName ? (
+              journaling ? (
+                <Redirect to="/Journal" />
+              ) : (
+                <Redirect to="/getInfo" />
+              )
             ) : (
-            <>
+              <Wrapper handleJournalClick={handleJournalClick} />
+            )}
+          </Route>
+          <Route exact path="/getInfo">
+            {gotUserName ? (
+              <Redirect to="/" />
+            ) : (
               <GetProfile
                 email={user.email}
                 handlePrfileInput={handleUserName}
                 handleProfileClick={submitProfile}
+                handleTagLineChange={handleTagLineChange}
               />
-            </>
-          )}
+            )}
+          </Route>
+        </Switch>
+        <Route exact path="/Journal">
+          <JournalWrapper
+            title={currentJournal.title}
+            handleTileChange={handleTileChange}
+            handleBodyChange={handleBodyChange}
+            handleSaveClick={handleSaveClick}
+            JournalEntries={entryList}
+          />
         </Route>
       </div>
-    </>
+    </Router>
   );
 };
 
 export default Profile;
-
-// if got got username is true
-
-{
-  /* <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-        <br />
-
-        {JSON.stringify(user, null, 6)}
-        <br /> */
-}
-
-{/* <>
-<Nav handleLogout={handleLogout} />
-<div className="container profile-body">
-  <Route exact path="/">
-    {gotUserName ? (
-      journaling ? (
-        <JournalWrapper
-          title={currentJournal.title}
-          handleTileChange={handleTileChange}
-          handleBodyChange={handleBodyChange}
-          handleSaveClick={handleSaveClick}
-          JournalEntries={entryList}
-        />
-      ) : (
-        <Wrapper
-          handleJournalClick={handleJournalClick}
-        />
-      )
-    ) : (
-      <>
-        <GetProfile
-          email={user.email}
-          handlePrfileInput={handleUserName}
-          handleProfileClick={submitProfile}
-        />
-      </>
-    )}
-  </Route>
-</div>
-</> */}
